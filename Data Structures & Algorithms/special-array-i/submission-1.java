904782d427e6298b963d1d29e17bcd106e32548c
gowri reddy <gowrireddy4868@gmail.com>
2026-09-08 19:10:39 +0530

    Add: special-array-i - submission-1

---
+++ Data Structures & Algorithms/special-array-i/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        boolean isSpecial=true;
+        int n=nums.length;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]%2==1&&nums[i+1]%2==1||nums[i]%2==0&&nums[i+1]%2==0){
+                isSpecial=false;
+                break;
+            }
+        }
+        return isSpecial;
+    }
+}
